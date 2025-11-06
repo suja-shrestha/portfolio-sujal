b1fba67520ef4b635ecc94cb4b782c3b2d92a0b3
suja-shrestha <shresthasujal917@gmail.com>
2025-11-06 14:09:25 +0545

    icone issue fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Nav from './component/nav/Nav.jsx'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
